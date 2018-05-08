4c58015a0177eae8166bf6c6a84521d60632adba
kml74123 <35539960+kml74123@users.noreply.github.com>
2018-05-08 16:33:09 -0500

    Add files via upload

---
+++ lab3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo blah blah > /dev/null
+
+echo "view permission"
+ls -l
+
+echo using the DD command
+dd if=/dev/zero of=new_file bs=1024 count=1
+
+sudo /lib/udev/ata_id --export /dev/sda
+
+udevadm info --query=all --name=/dev/sda
+
